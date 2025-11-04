39b6645a1569afe62dff12bed18148a6cba15b32
Tusshar <tushargawhade7@gmail.com>
2025-11-04 20:35:35 +0530

    initial commit

---
+++ Blogs_page/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Header from './components/Header'
+import Blogs from './components/Blogs'
+import Pageination from './components/Pageination'
+
+function App() {
+  return (
+    <div>
+        <Header/>
+        <Blogs/>
+        <Pageination/>
+
+    </div>
+  )
+}
+
+export default App
