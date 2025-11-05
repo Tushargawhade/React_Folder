0a6198b6439c2fff47b13bcaac0ae5a7942afc85
Tusshar <tushargawhade7@gmail.com>
2025-11-05 16:16:21 +0530

    create

---
+++ Blogs_page/src/components/Pageination.jsx
@@ -0,0 +1,12 @@
+    import React from 'react'
+    
+    function Pageination() {
+      return (
+        <div>
+            Pageination
+        </div>
+      )
+    }
+    
+    export default Pageination
+    
