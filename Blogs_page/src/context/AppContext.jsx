21da924e14aea6a0aba209085bd88f6ab8cf1f09
Tusshar <tushargawhade7@gmail.com>
2025-11-07 22:56:27 +0530

    initial commit

---
+++ Blogs_page/src/context/AppContext.jsx
@@ -0,0 +1,58 @@
+import { createContext, useState } from "react";
+import {baseUrl} from '../baseUrl'
+
+export const AppContext = createContext();
+
+function AppContextProvider({children}){
+
+    const [loading, setLoading] = useState(false);
+    const [posts , setPosts] = useState([]);
+    const [pages, setPages] =useState(1);
+    const [totalpages, setTotalPages] = useState(null);  
+
+
+    //data filling 
+
+    async function fetchBlogPosts(page=1){
+
+        setLoading(true);
+
+        let url = `${baseUrl}?page=${page}`
+
+        try{
+
+            const result = await fetch(url);
+            const data = await result.json();
+            console.log(data)
+
+
+        }
+        catch(err){
+            console.log("error in feting data!!");
+        }
+
+
+        setLoading(false);
+    }
+
+
+    const value = {
+        loading,
+        setLoading,
+        posts,
+        setPages,
+        setPosts,
+        pages,
+        totalpages,
+        setTotalPages
+    }
+
+
+
+
+    return <AppContext.Provider value={value}>
+               {children}
+             </AppContext.Provider>;    
+    
+
+}
