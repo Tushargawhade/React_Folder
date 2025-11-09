67692b3b16c9e26161ba67f3c001799dbb44faf5
Tusshar <tushargawhade7@gmail.com>
2025-11-09 23:04:29 +0530

    change

--- Blogs_page/src/App.jsx
+++ Blogs_page/src/App.jsx
@@ -3,12 +3,14 @@ import Header from './components/Header'
 import Blogs from './components/Blogs'
 import Pageination from './components/Pageination'
 
+
 function App() {
   return (
     <div>
         <Header/>
         <Blogs/>
         <Pageination/>
+
     </div>
   )
 }
